351027956440a1a4b9d780418141d2ca80288dfa
ttyler <ttyler@mac.com>
2020-08-13 11:30:06 +0800

    Add files via upload

---
+++ RenameMacCustom.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+	###############################################################
+	#	Copyright (c) 2019, D8 Services Ltd.  All rights reserved.  
+	#											
+	#	
+	#	THIS SOFTWARE IS PROVIDED BY D8 SERVICES LTD. "AS IS" AND ANY
+	#	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	#	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	#	DISCLAIMED. IN NO EVENT SHALL D8 SERVICES LTD. BE LIABLE FOR ANY
+	#	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	#	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	#	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	#	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	#	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	#	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	#
+	#
+	###############################################################
+	#
+
+#Only Run as Root
+thisUser=`whoami`
+if [[ $thisUser != "root" ]];then
+	echo "Can only run as root"
+	exit 1
+fi
+
+extensionBU=""
+JamfURL=`defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url`
+serialNumber=$(system_profiler SPHardwareDataType | awk '/Serial Number/{print $4}')
+
+IDNumber=$(echo ${serialNumber} | cut -c 6-12)
+#echo ${serialNumber}
+
+MacName="${extensionBU}${IDNumber}"
+
+if [[ -z ${MacName} ]];then
+	echo "One or more parameters are missing, exiting."
+	exit 2
+fi
+
+echo "MacName will be $MacName"
+echo "INFO: Resetting Computer Name to ${MacName}"
+scutil --set ComputerName "${MacName}"
+scutil --set LocalHostName "${MacName}"
+scutil --set HostName "${MacName}"
+jamf recon
